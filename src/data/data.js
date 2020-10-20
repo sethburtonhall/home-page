1add1491b321569f7309ea4c07d9d441c3f89e46
Seth Hall <sethburtonhall@gmail.com>
2020-10-20 11:41:18 -0400

    adds SoleLife, edits Ocupop

--- src/data/data.js
+++ src/data/data.js
@@ -10,38 +10,46 @@ import ushdotcom from "../images/ushahidi-dotcom.png"
 export const frontend = "Frontend"
 
 export const codeBio =
-  "I am a mid-level Frontend Engineer living in North Carolina. I enjoy the work and favor the JAMstack ecosystem. I currently work at Ocupop."
+  "I am a mid-level Frontend Engineer living in North Carolina. I enjoy the work and favor the JAMstack ecosystem. I currently work at SoleLife, Inc."
 
 export const filmBio =
   "I also have an MFA in Creative Producing from UNCSA. Hall Pictures is my Greensboro, NC based film company that specializes in the development and production of shorts, features, and series. We are truly independent, doing our best to develop great stories and produce high-quality content while providing a safe and nurturing environment for film creatives."
 
 export const jobs = [
-  {
-    title: `${frontend} Developer`,
-    company: "Ocupop",
-    description: `Ocupop is a creative design agency based out of Milwaukee, WI. As a member of the dev team, I've worked on a custom e-commerce platform and custom CMS marketing sites.`,
-    date: "May 2020-Present",
-    type: "Contract",
-    location: "Remote",
-  },
-  {
-    title: `${frontend} Engineer`,
-    company: "Ushahidi",
-    description: `Ushahidi is a Nairobi, Kenya based technology company that designs products and initiatives that strive to solve global problems. As part of the design team I was responsible for all front end code across multiple software products and websites.`,
-    date: "May 2013-October 2018",
-    type: "Full Time",
-    location: "Remote",
-  },
-  {
-    title: `${frontend} Engineer`,
-    company: "Self-Employed",
-    description:
-      "As a freelance web developer I have partnered with creative agencies and individuals on a variety of web projects ranging from custom builds to content management systems.",
-    date: "2011-2013 & 2018-2020",
-    type: "Freelance",
-    location: "Remote",
-  },
-]
+         {
+           title: `${frontend} Developer`,
+           company: "SoleLife, Inc",
+           description: `SoleLife streams and automates your coaching business with tele-conferencing, chat, admin tasks, scheduling, billing, and more.`,
+           date: "October 2020-Present",
+           type: "Contract",
+           location: "Remote",
+         },
+         {
+           title: `${frontend} Developer`,
+           company: "Ocupop",
+           description: `Ocupop is a creative design agency based out of Milwaukee, WI. As a member of the dev team, I worked on a custom e-commerce platform and custom CMS marketing sites.`,
+           date: "May 2020-September 2020",
+           type: "Contract",
+           location: "Remote",
+         },
+         {
+           title: `${frontend} Engineer`,
+           company: "Ushahidi",
+           description: `Ushahidi is a Nairobi, Kenya based technology company that designs products and initiatives that strive to solve global problems. As part of the design team I was responsible for all front end code across multiple software products and websites.`,
+           date: "May 2013-October 2018",
+           type: "Full Time",
+           location: "Remote",
+         },
+         {
+           title: `${frontend} Engineer`,
+           company: "Self-Employed",
+           description:
+             "As a freelance web developer I have partnered with creative agencies and individuals on a variety of web projects ranging from custom builds to content management systems.",
+           date: "2011-2013 & 2018-2020",
+           type: "Freelance",
+           location: "Remote",
+         },
+       ]
 
 export const hardSkills = [
   "HTML/CSS/JS",
